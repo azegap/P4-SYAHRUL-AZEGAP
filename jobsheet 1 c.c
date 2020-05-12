2072adce9ee9f5e8c4be362386ee91e89a7f6464
azegap <64589002+azegap@users.noreply.github.com>
2020-05-11 18:15:28 -0700

    Create jobsheet 1 c.c
    Buat program menampilkan NIM masing – masing mahasiswa.

---
+++ jobsheet 1 c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+printf (“syahrul azegap F1B019134 KLP 27”);	
+int nim[5]= {14,6,5,11,13,};
+	
+	printf ("Nim ke-1 = %i\n",nim[0]);
+	printf ("Nim ke-2 = %i\n",nim[1]);
+	printf ("Nim ke-3 = %i\n",nim[2]);
+	printf ("Nim ke-4 = %i\n",nim[3]);
+	printf ("Nim ke-5 = %i\n",nim[4]);
+
+}
