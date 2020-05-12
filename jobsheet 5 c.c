1c3c343481b1cb43427f238f7eec41ae4a0d871b
azegap <64589002+azegap@users.noreply.github.com>
2020-05-11 18:24:40 -0700

    Create jobsheet 5 c.c
    Perkalian matriks dua dimensi.

---
+++ jobsheet 5 c.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+	int nim1 [2][2]; 
+	int nim2 [2][2]; 
+	int hasil [2][2];
+	int x,y;
+		printf("Syahrul Azegap F1B019134 klp.27\n\n");
+	printf ("Perkalian Matrixs\n");
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			printf ("Masukkan nilai index %d, %d = ", x, y);
+			scanf ("%d", &nim1[x][y]);
+		}
+	}
+	printf ("\n");
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			printf ("Masukkan nilai index %d, %d = ", x, y);
+			scanf ("%d", &nim2[x][y]);
+		}
+	}
+	for (x = 0; x < 2; x++){
+		for (y = 0; y < 2; y++){
+			hasil [x][y]=nim1[x][y]*nim2[x][y];
+			printf ("%d ", hasil[x][y]);
+		}
+		printf ("\n");
+	}
+	getch();
+}
