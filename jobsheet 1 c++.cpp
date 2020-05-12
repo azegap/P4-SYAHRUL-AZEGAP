3245e0197b25ae7931407f0a298920f5085ab7b2
azegap <64589002+azegap@users.noreply.github.com>
2020-05-11 18:14:36 -0700

    Create jobsheet 1 c++.cpp
    Buat program menampilkan NIM masing – masing mahasiswa.

---
+++ jobsheet 1 c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<("syahrul azegap F1B019134\n");
+	cout<<("kelompok 27\n");
+	int nim[5]= {14,6,5,11,13,};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
