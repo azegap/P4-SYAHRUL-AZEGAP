1e31213627de64d0fa2e80c3d54ca66e6c55d45e
azegap <64589002+azegap@users.noreply.github.com>
2020-05-11 18:26:01 -0700

    Create jobsheet 6 c.c
    Matriks transpose.

---
+++ jobsheet 6 c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+	int nim1 [5][5]; 
+	int i, j, x, y;
+      printf("Syahrul Azegap F1B019134 klp.27\n\n");
+	printf ("MATRIX TRANSPOSE\n");
+	printf ("Masukkan jumlah baris = ");
+	scanf ("%i", &x);
+	printf ("Masukkan jumlah kolom = ");
+	scanf ("%i", &y);
+	printf ("MATRIX \n");
+	for (i = 0; i < x; i++){
+		for (j = 0; j < y; j++){
+			printf ("Masukkan nilai index %d, %d = ", i, j);
+			scanf ("%d", &nim1[i][j]);
+		}
+	}
+	for (i = 0; i < x; i++){
+		for (j = 0; j < y; j++){
+			printf ("%d ",nim1[i][j]);
+		}
+		printf ("\n");
+	}
+	printf ("TRANSPOSE MATRIX  \n");
+	for (i = 0; i < y; i++){
+		for (j = 0; j < x; j++){
+			printf ("%d ", nim1[j][i]);
+		}
+	printf ("\n");
+	}
+	getch();
+}
